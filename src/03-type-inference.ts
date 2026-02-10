449b08a5d58c7ba0ea99341f7d4b6fab57c5643b
poorna.narsingosu <poornachary1@gmail.com>
2026-02-10 18:40:05 +0530

    inferred types in ts

---
+++ src/03-type-inference.ts
@@ -0,0 +1,6 @@
+let inferredType = 'poorna';
+
+//TypeScript automatically figures out the type for you based on the value.
+// if i try to add any new value to infferedtype it will give error.
+// inferredType = 10; //error
+// Type 'number' is not assignable to type 'string'.ts(2322)
